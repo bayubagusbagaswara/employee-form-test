ccc0f11936cc83b7b86e40f680067421d714999a
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 21:57:56 +0700

    update model EmployeeTrainingHistory

--- src/main/java/com/bayu/employee/model/EmployeeTrainingHistory.java
+++ src/main/java/com/bayu/employee/model/EmployeeTrainingHistory.java
@@ -1,30 +1,34 @@
 package com.bayu.employee.model;
 
-import com.bayu.employee.model.audit.UserDateAudit;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
-@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "employee_training_history")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmployeeTrainingHistory extends UserDateAudit {
+public class EmployeeTrainingHistory {
 
     @Id
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     @GeneratedValue(generator = "system-uuid")
     private String id;
 
+    @Column(name = "course_name")
+    private String courseName;
+
+    @Column(name = "certificate")
+    private String certificate;
+
+    @Column(name = "year")
+    private String year;
+
     @ManyToOne
     @JoinColumn(name = "id_employee", foreignKey = @ForeignKey(name = "fk_training_history_id_employee"), referencedColumnName = "id")
     private Employee employee;
 
-    // namaKursus
-    // sertifikat
-    // tahun
 }
