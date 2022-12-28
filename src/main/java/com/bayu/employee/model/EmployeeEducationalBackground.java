0e7076c392f3130da4fc6483b685cd5f34a51149
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 10:55:30 +0700

    add model EmployeeEducationalBackground

---
+++ src/main/java/com/bayu/employee/model/EmployeeEducationalBackground.java
@@ -0,0 +1,29 @@
+package com.bayu.employee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee_educational_background")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeEducationalBackground {
+
+    @Id
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    @GeneratedValue(generator = "system-uuid")
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_employee", foreignKey = @ForeignKey(name = "fk_educational_background_id_employee"), referencedColumnName = "id")
+    private Employee employee;
+}
+
+
