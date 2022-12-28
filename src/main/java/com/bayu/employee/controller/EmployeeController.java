f25337f78f8cf30558affdd04eebb91e93f613e1
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 13:28:27 +0700

    update EmployeeController

--- src/main/java/com/bayu/employee/controller/EmployeeController.java
+++ src/main/java/com/bayu/employee/controller/EmployeeController.java
@@ -1,4 +1,23 @@
 package com.bayu.employee.controller;
 
+import com.bayu.employee.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+
+    // handle to create new employee
+
+    // handle to edit employee
+
+    // handle to get employee by id
+
+    // handle to get all employees
+
+    // handle to delete employee by id
 }
