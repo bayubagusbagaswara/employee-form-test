eef235a686389990f9c43aa5a1b31cbc7925c1a9
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 13:28:37 +0700

    update AuthenticationController

--- src/main/java/com/bayu/employee/controller/AuthenticationController.java
+++ src/main/java/com/bayu/employee/controller/AuthenticationController.java
@@ -1,4 +1,18 @@
 package com.bayu.employee.controller;
 
+import com.bayu.employee.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final UserService userService;
+
+    // handle to register user (create a new user)
+
+    // handle to login user
+
+    // handle to logout user
 }
