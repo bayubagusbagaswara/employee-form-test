bcfa002ae3357e8c810522694fd761dcd1559329
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 21:58:03 +0700

    update model EmployeeWorkExperience

--- src/main/java/com/bayu/employee/model/EmployeeWorkExperience.java
+++ src/main/java/com/bayu/employee/model/EmployeeWorkExperience.java
@@ -1,31 +1,37 @@
 package com.bayu.employee.model;
 
-import com.bayu.employee.model.audit.UserDateAudit;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
-@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "employee_work_experience")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmployeeWorkExperience extends UserDateAudit {
+public class EmployeeWorkExperience {
 
     @Id
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     @GeneratedValue(generator = "system-uuid")
     private String id;
 
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "last_position")
+    private String lastPosition;
+
+    @Column(name = "last_income")
+    private String lastIncome;
+
+    @Column(name = "year")
+    private String year;
+
     @ManyToOne
     @JoinColumn(name = "id_employee", foreignKey = @ForeignKey(name = "fk_work_experience_id_employee"), referencedColumnName = "id")
     private Employee employee;
 
-    // namaPerusahaan
-    // posisiTerakhir
-    // pendapatanTerakhir
-    // tahun
 }
