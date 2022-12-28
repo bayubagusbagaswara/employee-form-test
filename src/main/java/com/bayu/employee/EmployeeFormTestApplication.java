178870153a3509510b7eb1e0307f2a4a6c9a3cbc
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 09:56:01 +0700

    Initial commit

---
+++ src/main/java/com/bayu/employee/EmployeeFormTestApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeFormTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeFormTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/employee/EmployeeFormTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeFormTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
