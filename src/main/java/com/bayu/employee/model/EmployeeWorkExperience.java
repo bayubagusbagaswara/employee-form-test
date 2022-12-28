62045d57c364b5fb5d5a4004a9d49764d74cae84
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 10:55:47 +0700

    add model EmployeeWorkExperience

---
+++ src/main/java/com/bayu/employee/model/EmployeeWorkExperience.java
@@ -0,0 +1,26 @@
+package com.bayu.employee.model;
+
+import com.bayu.employee.model.audit.UserDateAudit;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "employee_work_experience")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeWorkExperience extends UserDateAudit {
+
+    @Id
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    @GeneratedValue(generator = "system-uuid")
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_employee", foreignKey = @ForeignKey(name = "fk_work_experience_id_employee"), referencedColumnName = "id")
+    private Employee employee;
+}
