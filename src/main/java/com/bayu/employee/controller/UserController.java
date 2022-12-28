2336421c40ae104204613bc8467de62d076bbdf7
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 13:28:15 +0700

    update UserController

--- src/main/java/com/bayu/employee/controller/UserController.java
+++ src/main/java/com/bayu/employee/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.bayu.employee.controller;
 
+import com.bayu.employee.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+
+    // handle to register user (create new user)
+
+    // handle to l
 }
