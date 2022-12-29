78128fa11df209a8142d28bafbe3344d57c5882a
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-29 08:53:15 +0700

    update EmployeeController

--- src/main/java/com/bayu/employee/controller/EmployeeController.java
+++ src/main/java/com/bayu/employee/controller/EmployeeController.java
@@ -3,15 +3,22 @@ package com.bayu.employee.controller;
 import com.bayu.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/employee")
 @RequiredArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-
     // handle to create new employee
+
+    @GetMapping()
+    public String showFormAddEmployee() {
+        return "";
+    }
 
     // handle to edit employee
 
