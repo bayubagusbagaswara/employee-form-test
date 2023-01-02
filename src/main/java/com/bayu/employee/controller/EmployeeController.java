ada3bdbb4a5c7efed243c0e09794b842158c5ffd
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-02 09:13:05 +0700

    update controller EmployeeController

--- src/main/java/com/bayu/employee/controller/EmployeeController.java
+++ src/main/java/com/bayu/employee/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.bayu.employee.controller;
 
+import com.bayu.employee.model.Employee;
 import com.bayu.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,12 +16,30 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    // handle to create new employee
 
-    @GetMapping()
+    // menampilkan form untuk Add Employee
+    @GetMapping("/show-form-employee")
     public String showFormAddEmployee() {
         return "";
     }
+
+    // melakukan aksi penyimpanan Employee baru
+    @PostMapping("/save-employee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        return "";
+    }
+
+    // menampilkan form untuk Add Employee Educational Background
+
+    // melakukan aksi penyimpanan Employee Educational Background
+
+    // menampilkan form untuk Add Employee Training History
+
+    // melakukan aksi penyimpanan Employee Training History
+
+    // menampilkan form untuk Add Employee Work Experience
+
+    // melalukan aksi P
 
     // handle to edit employee
 
